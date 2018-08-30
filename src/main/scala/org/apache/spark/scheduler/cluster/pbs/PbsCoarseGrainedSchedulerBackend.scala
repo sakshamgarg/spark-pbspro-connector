04b62e1032dc67d41b52885894fa598c7dabab52
Utkarsh <utkarsh.maheshwari@altair.com>
2018-08-30 19:18:58 +0530

    WIP: Implement SchedulerDriver

--- src/main/scala/org/apache/spark/scheduler/cluster/pbs/PbsCoarseGrainedSchedulerBackend.scala
+++ src/main/scala/org/apache/spark/scheduler/cluster/pbs/PbsCoarseGrainedSchedulerBackend.scala
@@ -12,87 +12,20 @@ private[spark] class PbsCoarseGrainedSchedulerBackend(
     masterURL: String)
   extends CoarseGrainedSchedulerBackend(scheduler.asInstanceOf[TaskSchedulerImpl], sc.env.rpcEnv) {
 
+  val driver = new PbsSchedulerDriver()
+
+  /**
+   * Create and start a scheduler driver
+   */
   override def start() {
-    // TODO
+    super.start()
+    driver.start()
   }
 
-  override def sufficientResourcesRegistered(): Boolean = {
-    false
-    // TODO
-  }
-
-  /* NOTE: Replace all org.apache.mesos thingies with Pbs ones.
-
-  override def offerRescinded(d: org.apache.mesos.SchedulerDriver, o: OfferID) {}
-
-  override def registered(
-      driver: org.apache.mesos.SchedulerDriver,
-      frameworkId: FrameworkID,
-      masterInfo: MasterInfo) {
-    // TODO
-  }
-
-
-  override def disconnected(d: org.apache.mesos.SchedulerDriver) {
-    // TODO
-  }
-
-  override def reregistered(d: org.apache.mesos.SchedulerDriver, masterInfo: MasterInfo) {
-    // TODO
-  }
-
-  override def resourceOffers(d: org.apache.mesos.SchedulerDriver, offers: JList[Offer]) {
-    // TODO
-  }
-
-  override def statusUpdate(d: org.apache.mesos.SchedulerDriver, status: TaskStatus) {
-    // TODO
-  }
-
-  override def error(d: org.apache.mesos.SchedulerDriver, message: String) {
-    // TODO
-  }
-  */
-
+  /**
+   * Stop the scheduler driver
+   */
   override def stop() {
-    // TODO
-  }
-
-  /* NOTE: SlaveID and ExecutorID are the internal ids for mesos. We need something similar for pbs.
-  override def frameworkMessage(
-      d: org.apache.mesos.SchedulerDriver, e: ExecutorID, s: SlaveID, b: Array[Byte]) {}
-
-  override def slaveLost(d: org.apache.mesos.SchedulerDriver, slaveId: SlaveID): Unit = {
-    null
-    // TODO
-  }
-
-  override def executorLost(
-      d: org.apache.mesos.SchedulerDriver, e: ExecutorID, s: SlaveID, status: Int): Unit = {
-    null
-    // TODO
-  }
-  */
-
- /* NOTE: No idea what this is!
-  override def applicationId(): String = Option(appId).getOrElse {
-    null
-    // TODO
-  }
-  */
-
-  override def doRequestTotalExecutors(requestedTotal: Int): Future[Boolean] = Future.successful {
-    false
-    // TODO
-  }
-
-  override def doKillExecutors(executorIds: Seq[String]): Future[Boolean] = Future.successful {
-    false
-    // TODO
-  }
-
-  override def fetchHadoopDelegationTokens(): Option[Array[Byte]] = {
-    null
     // TODO
   }
 
---
+++ src/main/scala/org/apache/spark/scheduler/cluster/pbs/PbsSchedulerDriver.scala
@@ -0,0 +1,5 @@
+package org.apache.spark.scheduler.cluster.pbs
+
+private[pbs] class PbsSchedulerDriver() {
+  def start() {}
+}
