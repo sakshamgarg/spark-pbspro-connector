f5c4161b9113d0f689ef15dea4ef499315be7cce
Utkarsh <utkarsh.maheshwari@altair.com>
2018-09-03 19:06:06 +0530

    Complete a simple organization for scheduler

--- src/main/scala/org/apache/spark/scheduler/cluster/pbs/PbsCoarseGrainedSchedulerBackend.scala
+++ src/main/scala/org/apache/spark/scheduler/cluster/pbs/PbsCoarseGrainedSchedulerBackend.scala
@@ -6,27 +6,57 @@ import org.apache.spark.{SecurityManager, SparkConf, SparkContext}
 import org.apache.spark.scheduler.{TaskScheduler, TaskSchedulerImpl}
 import org.apache.spark.scheduler.cluster.CoarseGrainedSchedulerBackend
 
+import org.pbspro.pbs.Offer
+
 private[spark] class PbsCoarseGrainedSchedulerBackend(
     scheduler: TaskScheduler,
     sc: SparkContext,
     masterURL: String)
   extends CoarseGrainedSchedulerBackend(scheduler.asInstanceOf[TaskSchedulerImpl], sc.env.rpcEnv) {
 
-  val driver = new PbsSchedulerDriver()
+  private val driver = new PbsSchedulerDriver()
 
   /**
-   * Create and start a scheduler driver
+   * Start and initialize a scheduler driver.
    */
   override def start() {
+    logInfo("Starting PBS Scheduler backend")
     super.start()
-    driver.start()
+    driver.init()
   }
 
   /**
-   * Stop the scheduler driver
+   * Stop the scheduler driver.
    */
   override def stop() {
-    // TODO
+    logInfo("Stopping PBS Scheduler backend")
+  }
+
+  /**
+   * Kill the executors in the given list.
+   *
+   * @param executorIds list of Executor IDs to kill
+   * @return if all the Executors were killed
+   */
+  override def doKillExecutors(executorIds: Seq[String]): Future[Boolean] = Future.successful {
+    for (executor <- executorIds) {
+      driver.killExecutor(executor)
+    }
+    true
+  }
+
+  /**
+   * Notify the scheduler about the number of Executors wanted by the application.
+   *
+   * @param requestedTotal number of Executors wanted (including already allocated)
+   * @return if the request is acknowledged
+   */
+  override def doRequestTotalExecutors(requestedTotal: Int): Future[Boolean] = Future.successful {
+    logInfo(requestedTotal + " executors requested")
+    for (i <- 0 to requestedTotal) {
+      driver.createExecutor()
+    }
+    true
   }
 
 }
--- src/main/scala/org/apache/spark/scheduler/cluster/pbs/PbsSchedulerDriver.scala
+++ src/main/scala/org/apache/spark/scheduler/cluster/pbs/PbsSchedulerDriver.scala
@@ -3,5 +3,29 @@ package org.apache.spark.scheduler.cluster.pbs
 import org.pbspro.pbs.PBS
 
 private[pbs] class PbsSchedulerDriver() {
-  def start() {}
+
+  /**
+   * Register with pbs and initialize stuff.
+   */
+  def init() {
+  }
+
+  /**
+   * Submit a qsub which will act as an Executor.
+   *
+   * @return The Executor ID
+   */
+  def createExecutor(): String = {
+    null
+  }
+
+  /**
+   * Kill the Executor job.
+   *
+   * @param id ID of the executor
+   * @return if the Executor was killed
+   */
+  def killExecutor(id: String): Boolean = {
+    true
+  }
 }
