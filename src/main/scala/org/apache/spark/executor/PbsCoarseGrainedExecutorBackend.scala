3445f85c6308c553a00211a8a13cef5e82ae8ded
Utkarsh <utkarsh.maheshwari@altair.com>
2018-09-05 17:37:18 +0530

    Change ExecutorBackend to CoarseGrained

---
+++ src/main/scala/org/apache/spark/executor/PbsCoarseGrainedExecutorBackend.scala
@@ -0,0 +1,38 @@
+package org.apache.spark.executor
+
+import java.nio.ByteBuffer
+import java.net.URL
+
+import org.apache.spark.{SparkConf, SparkEnv, TaskState}
+import org.apache.spark.internal.Logging
+import org.apache.spark.rpc._
+
+private[spark] class PbsCoarseGrainedExecutorBackend(
+    rpcEnv: RpcEnv,
+    driverUrl: String,
+    executorId: String,
+    hostname: String,
+    cores: Int,
+    userClassPath: Seq[URL],
+    env: SparkEnv)
+  extends CoarseGrainedExecutorBackend(
+    rpcEnv,
+    driverUrl,
+    executorId,
+    hostname,
+    cores,
+    userClassPath,
+    env)
+  with Logging {
+
+  //override def statusUpdate(taskId: Long, state: TaskState.TaskState, data: ByteBuffer) {
+  //}
+
+}
+
+/*
+ * TODO: Mesos has an object which is the "entry point" for executor, as follows:
+ *    private[spark] object MesosExecutorBackend extends Logging {
+ *
+ * Find out what has to be done with this shit.
+ */
