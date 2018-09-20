3b8967c06b61163778436e36a19d8fe7d9a3f092
Utkarsh <utkarsh.maheshwari@altair.com>
2018-09-20 17:32:46 +0530

    Refactor Client to use Utils.qsub

--- src/main/scala/org/apache/spark/deploy/pbs/Client.scala
+++ src/main/scala/org/apache/spark/deploy/pbs/Client.scala
@@ -1,48 +1,52 @@
 package org.apache.spark.deploy.pbs
 
 import java.io.File
-import scala.sys.process._
 
-import org.apache.spark.SparkConf
+import org.apache.spark.{SparkConf, SparkException}
 import org.apache.spark.internal.Logging
-import org.apache.spark.rpc.RpcEndpointAddress
-import org.apache.spark.scheduler.cluster.CoarseGrainedSchedulerBackend
+
+import org.apache.spark.pbs.Utils
 
 private[pbs] class Client(args: ClientArguments, sparkConf: SparkConf) extends Logging {
+
+  private val defaultDriverCores: Int = 1
+  private val defaultDriverMemory: String = "1G"
+  private val defaultAppName: String = "spark-driver"
 
   /**
    * This is responsible to build and start a driver program on a node in the cluster. This driver
    * then builds the SparkContext and starts the application to which other Executors connect.
    *
-   * Our object is to get the spark-submit to a node in the cluster. This can be achieved if we
+   * Our objective is to get the spark-submit to a node in the cluster. This can be achieved if we
    * simply qsub the spark-submit command, but in client mode so that the driver is started on
    * whatever node the qsub went for execution. The driver which will be started will handle the
    * launching of executors on its own.
    */
   def run() {
-
-    val sparkHome = sparkConf.getOption("spark.home") match {
-      case Some(home) =>
-        home
-      case None =>
-        "/home/utkmah/code/spark/"  // FIXME
+    val sparkHome: String = sparkConf.getOption("spark.executor.pbs.home")
+      .orElse(sparkConf.getOption("spark.home"))
+      .getOrElse {
+        throw new SparkException("Executor Spark home `spark.executor.pbs.home` not set!")
+      }
+    val appName: String = sparkConf.getOption("spark.app.name")
+      .getOrElse(defaultAppName)
+      .replaceAll("\\s", "")
+    val driverCores: Int = sparkConf.getOption("spark.driver.cores") match {
+      case Some(cores) => cores.toInt
+      case None => defaultDriverCores
     }
+    val driverMemory: String = sparkConf.getOption("spark.driver.memory")
+      .getOrElse(defaultDriverMemory)
+    val runScript: String = new File(sparkHome, "bin/spark-submit").getPath
 
-    val appName = sparkConf.getOption("spark.app.name").getOrElse("spark-driver")
-    val appId = appName  // TODO
+    val opts: String = "--master pbs --deploy-mode client" +
+        s" --driver-cores $driverCores --driver-memory $driverMemory" +
+        s" --class ${args.mainClass} ${args.primaryJavaResource} ${args.arg}"
+        s"" // TODO: Configure the driver command for python, R etc.
 
-    val driverMemory = sparkConf.getOption("spark.driver.memory").getOrElse("1g")
-    val driverCores = sparkConf.getOption("spark.driver.cores").getOrElse("1")
-    val runScript = new File(sparkHome, "bin/spark-submit").getPath
+    val driverCommand = s"$runScript $opts"
 
-    // TODO: Configure the driver command for python, R etc.
-    val driverCommand = s"$runScript --master pbs --deploy-mode client " +
-        s"--driver-cores $driverCores --driver-memory $driverMemory " +
-        s"--class ${args.mainClass} ${args.primaryJavaResource} ${args.arg}"
-    val cmd = s"qsub -N $appName -l select=1:ncpus=$driverCores:mem=$driverMemory -- $driverCommand"
-
-    logInfo(s"Starting driver: $cmd")
-    val out = cmd.!!
-
+    logInfo(s"Starting driver: $driverCommand")
+    Utils.qsub(appName, driverCores, driverMemory, driverCommand)
   }
 }
