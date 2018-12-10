0e147baa530ba52255ea6771dae9620e761be08c
Utkarsh Maheshwari <utkarshme96@gmail.com>
2018-12-10 13:30:45 +0530

    testing

---
+++ dev/install_deps.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+git clone https://github.com/pbspro/pbspro /tmp/pbspro
+cd /tmp/pbspro
+./autogen.sh
+./configure  --prefix /opt/pbs
+make
+sudo make install
+sudo /opt/pbs/libexec/pbs_postinstall
+sudo vi /etc/pbs.conf
+sudo chmod 4755 /opt/pbs/sbin/pbs_iff /opt/pbs/sbin/pbs_rcp
+sudo /etc/init.d/pbs start
+source /etc/profile.d/pbs.sh
+
+qstat
