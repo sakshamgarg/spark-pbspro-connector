e9f013b71fe932f8775ebfe5d41e74dad0bc181c
Utkarsh Maheshwari <utkarshme96@gmail.com>
2018-12-11 18:32:50 +0530

    Remove echos and check for pbs server connection

--- dev/install_deps.sh
+++ dev/install_deps.sh
@@ -15,32 +15,14 @@ cd /tmp/pbspro
 make
 sudo make install
 
-echo "install done"
-
 sudo /opt/pbs/libexec/pbs_postinstall
-echo "postinstall done"
-
 sudo chmod 4755 /opt/pbs/sbin/pbs_iff /opt/pbs/sbin/pbs_rcp
-echo "chmod done"
+
+/opt/pbs/sbin/pbs_iff -t $(hostname) 15001 || quit "PBSPro could not connect to the server"
 
 sudo sh -c "echo \"PBS_SERVER=$(hostname)\" >> /etc/pbs.conf"
-echo "echo shit done"
-
-cat /etc/hosts
-
-ping -c 5 localhost
-ping -c 5 $(hostname)
-
-ls -l /opt/pbs/sbin/pbs_iff
-sudo /opt/pbs/libexec/pbs_habitat || quit "habitat failed"
-echo "pbs habitat done"
-
 sudo /etc/init.d/pbs start || quit "Could not start PBS"
-echo "starting done"
 
 . /etc/profile.d/pbs.sh || quit "Could not source profile.d/pbs.sh"
-echo "sourcing done"
-
 qstat || quit "Could not qstat"
 
-echo "Done"
