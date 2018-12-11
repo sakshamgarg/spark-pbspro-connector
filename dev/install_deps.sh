827b2dcd705452e2cd7d4c05e5b948a2636b09db
Utkarsh Maheshwari <utkarshme96@gmail.com>
2018-12-11 15:35:31 +0530

    dont start postgre

--- dev/install_deps.sh
+++ dev/install_deps.sh
@@ -2,14 +2,19 @@
 
 git clone https://github.com/pbspro/pbspro /tmp/pbspro
 cd /tmp/pbspro
+
 ./autogen.sh
 ./configure  --prefix /opt/pbs
+
 make
 sudo make install
+
 sudo /opt/pbs/libexec/pbs_postinstall
-#sudo vi /etc/pbs.conf
 sudo chmod 4755 /opt/pbs/sbin/pbs_iff /opt/pbs/sbin/pbs_rcp
+
+#sudo /etc/init.d/postgresql start
 sudo /etc/init.d/pbs start
+
 . /etc/profile.d/pbs.sh
 
 qstat
