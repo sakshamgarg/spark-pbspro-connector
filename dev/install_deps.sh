b9daaadd31b1c13b510fc1161022b6f6dfa705d9
Utkarsh Maheshwari <utkarshme96@gmail.com>
2018-12-11 17:13:22 +0530

    make it verbose

--- dev/install_deps.sh
+++ dev/install_deps.sh
@@ -15,14 +15,22 @@ cd /tmp/pbspro
 make
 sudo make install
 
+echo "install done"
+
 sudo /opt/pbs/libexec/pbs_postinstall
+echo "postinstall done"
+
 sudo chmod 4755 /opt/pbs/sbin/pbs_iff /opt/pbs/sbin/pbs_rcp
+echo "chmod done"
 
 sudo sh -c "echo \"PBS_SERVER=$(hostname)\" >> /etc/pbs.conf"
+echo "echo shit done"
 
 sudo /etc/init.d/pbs start || quit "Could not start PBS"
+echo "starting done"
 
 . /etc/profile.d/pbs.sh || quit "Could not source profile.d/pbs.sh"
+echo "sourcing done"
 
 qstat || quit "Could not qstat"
 
