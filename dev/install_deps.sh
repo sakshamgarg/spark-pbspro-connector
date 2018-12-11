e6f78ecf14827a91a976163441044820877eabc9
Utkarsh Maheshwari <utkarshme96@gmail.com>
2018-12-11 14:34:43 +0530

    Don't open conf file in vi

--- dev/install_deps.sh
+++ dev/install_deps.sh
@@ -7,9 +7,9 @@ cd /tmp/pbspro
 make
 sudo make install
 sudo /opt/pbs/libexec/pbs_postinstall
-sudo vi /etc/pbs.conf
+#sudo vi /etc/pbs.conf
 sudo chmod 4755 /opt/pbs/sbin/pbs_iff /opt/pbs/sbin/pbs_rcp
 sudo /etc/init.d/pbs start
-#source /etc/profile.d/pbs.sh
+source /etc/profile.d/pbs.sh
 
 qstat
