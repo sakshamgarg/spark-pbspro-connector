8169f55243511cace04cb02b81734fb081205940
Utkarsh Maheshwari <utkarshme96@gmail.com>
2018-12-11 16:49:57 +0530

    echo hostname

--- dev/install_deps.sh
+++ dev/install_deps.sh
@@ -18,6 +18,8 @@ sudo make install
 sudo /opt/pbs/libexec/pbs_postinstall
 sudo chmod 4755 /opt/pbs/sbin/pbs_iff /opt/pbs/sbin/pbs_rcp
 
+sudo sh -c "echo \"PBS_SERVER $(hostname)\" >> /etc/pbs.conf"
+
 sudo /etc/init.d/pbs start || quit "Could not start PBS"
 
 . /etc/profile.d/pbs.sh || quit "Could not source profile.d/pbs.sh"
