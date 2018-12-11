3617924554079dc50ca19b50b01aafaccd2f1aa9
Utkarsh Maheshwari <utkarshme96@gmail.com>
2018-12-11 17:02:22 +0530

    fix pbs_server

--- dev/install_deps.sh
+++ dev/install_deps.sh
@@ -18,7 +18,7 @@ sudo make install
 sudo /opt/pbs/libexec/pbs_postinstall
 sudo chmod 4755 /opt/pbs/sbin/pbs_iff /opt/pbs/sbin/pbs_rcp
 
-sudo sh -c "echo \"PBS_SERVER $(hostname)\" >> /etc/pbs.conf"
+sudo sh -c "echo \"PBS_SERVER=$(hostname)\" >> /etc/pbs.conf"
 
 sudo /etc/init.d/pbs start || quit "Could not start PBS"
 
