a8dc8db4f7637bdfc6a230d4ff14b8306d9323a5
Utkarsh Maheshwari <utkarshme96@gmail.com>
2018-12-13 19:21:49 +0530

    Change scp

--- dev/install_deps.sh
+++ dev/install_deps.sh
@@ -17,9 +17,9 @@ sudo make install
 sudo /opt/pbs/libexec/pbs_postinstall
 sudo chmod 4755 /opt/pbs/sbin/pbs_iff /opt/pbs/sbin/pbs_rcp
 
-/opt/pbs/sbin/pbs_iff -t $(hostname) 15001 || quit "PBSPro could not connect to the server"
-
 sudo sh -c "echo \"PBS_SERVER=$(hostname)\" >> /etc/pbs.conf"
+sudo sh -c "echo \"PBS_SCP=$(which scp)\" >> /etc/pbs.conf"
+
 sudo /etc/init.d/pbs start || quit "Could not start PBS"
 
 . /etc/profile.d/pbs.sh || quit "Could not source profile.d/pbs.sh"
