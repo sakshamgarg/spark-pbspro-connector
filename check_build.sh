5ffe69a3530448b879f0c63bd7f93c9fa09a02cc
Saksham Garg <garg.saksham@yahoo.com>
2020-04-06 09:03:31 +0530

    Update patch to match with latest spark (#27)

--- check_build.sh
+++ check_build.sh
@@ -1,33 +1,29 @@
-#!/bin/sh
+#!/bin/sh -x
 
 spark_repo="https://github.com/apache/spark"
-pbs_repo="https://github.com/PBSPro/spark-pbspro-connector"
-
-cleanup() {
-  cd /
-  rm -rf /tmp/spark
-}
 
 quit() {
   echo "$1"
-  cleanup
   exit 1
 }
 
+if [ -z "$TRAVIS_BUILD_DIR" ]; then
+	pbs_patch_dir=$(pwd)
+else
+	pbs_patch_dir=$TRAVIS_BUILD_DIR
+fi
 
 # clone spark
 cd /tmp
 git clone $spark_repo                       || quit "Unable to clone spark repo"
 cd spark
 
-# clone pbs
-git clone $pbs_repo resource-managers/pbs   || quit "Unable to clone pbs repo"
+# copy pbs_patch_dir
+mv $pbs_patch_dir resource-managers/pbs     || quit "Unable to copy pbs_patch repo"
 
 # apply patches
 git am resource-managers/pbs/*.patch        || quit "Unable to apply patch to spark"
 
 # build
 export MAVEN_SKIP_RC=1
-build/mvn -q -DskipTests -Ppbs package      || quit "Unable to build spark with pbs"
-
-cleanup
+build/mvn -q -DskipTests -Ppbs clean package      || quit "Unable to build spark with pbs"
